3452e3fe9068260f1d333d5099e211509f42a210
yanis lamliji <yanis.lamliji@ynov.com>
2025-05-20 00:54:30 +0200

    Update

--- Startloop.sh
+++ Startloop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+docker stop guessword || true
+docker rm guessword || true
+
 docker build -t guessword .
-docker run -p 80:80 guessword
+docker -d --name guessword run -p 80:80 guessword
