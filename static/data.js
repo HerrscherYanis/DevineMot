29eb73fa1c380f24f937260c7155f75a55841ee2
yanis lamliji <yanis.lamliji@ynov.com>
2025-05-19 09:23:53 +0200

    Update

--- static/data.js
+++ static/data.js
@@ -7,11 +7,12 @@ function Relocalize(){
 }
 
 function begin(){
-    difficult = sessionStorage.getItem("difficulty").split("/")
-    console.log()
+    difficult = sessionStorage.getItem("difficulty").split("/");
     if(difficult[difficult.length - 1] == "easy"){x = 10*2}
     else if(difficult[difficult.length - 1] == "medium"){x = Math.floor(10*1.5)}
     else if(difficult[difficult.length - 1] == "hard"){x = 10*1}
     else{x = Math.floor(10*0.1)}
     document.getElementById("congrat").innerHTML = `<p>Bravo Vous avez trouver le mot en ${x - sessionStorage.getItem("life")} essaie.</p>`
+    document.getElementById("logs").innerHTML = `<p>${sessionStorage.getItem("log")}</p>`
+    console.log(sessionStorage.getItem("log"));
 }
--- static/game.js
+++ static/game.js
@@ -1,27 +1,34 @@
 function Result(word){
-    StoreInLog()
     const rep = document.getElementById("rep");
     const life = document.getElementById("life");
     const log = document.getElementById("log");
-    if(word == rep.value){
-        Win(life.textContent)
-    }
-    else{
-        if(verifySpe(rep.value) == false){
+    if(verifySpe(rep.value) == false){
+        life.innerHTML = life.textContent - 1
         let [valid, comment] = verifyRep(word,rep.value)
         log.innerHTML += `<p>${valid} chiffre bonne, ${rep.value} ${comment}\n<p>`;
-        life.innerHTML = life.textContent - 1
+        Win(life.textContent,log.textContent,word,rep.value)
         GameOver(life.textContent,log.textContent,word)
-        }
     }
 }
 
-function StoreInLog(){
-    const list = sessionStorage.getItem("log")
-    console.log(list)
-    if(list == null)
-        console.log("True")
-}
+// function StoreInLog(word){
+//     list = sessionStorage.getItem("log");
+//     console.log(list);
+//     if(list == null){
+//         delete list;
+//         sessionStorage.setItem("log", []);
+//         list = sessionStorage.getItem("log");
+//     }
+//     x = []
+//     for (let i = 0; i < list.length; i++) {
+//         console.log(list[i]);
+//         x.push(list[i])
+//     }
+//     x.push(word)
+//     sessionStorage.setItem("log", x);
+//     console.log(x);
+// }
+
 
 function letterOnly(input){
     input.value = input.value.replace(/[^a-z]$/,"")
@@ -49,9 +56,13 @@ function verifySpe(word){
     }
     return false
 }
-function Win(life){
-    sessionStorage.setItem("life", life);
-    location.replace("/game/win")
+function Win(life,log,word,rep){
+    if(word == rep){
+        sessionStorage.setItem("life", life);
+        sessionStorage.setItem("log", log);
+        sessionStorage.setItem("difficulty", window.location.href);
+        location.replace("/game/win")
+    }
 }
 function GameOver(life,log,word){
     if(life <= 0){
@@ -60,35 +71,3 @@ function GameOver(life,log,word){
         location.replace("/game/finality")
     }
 }
-// function insinuate(word_user, length){
-//     verify = word_user
-//     if(length == -1){
-//         verify = self.word_search
-//     }
-//     for x in range(len(verify)):
-//         if self.word_search[x] == word_user[x]:
-//             hint.append(1)
-//         else:
-//             hint.append(0)
-    
-//     return hint
-// }
-             
-// function world_length(self, word_user):
-//         if len(self.word_search) > len(word_user):
-//             return {"code" : 1, "comment" : "Votre mot est trop Petit."}
-//         elif len(self.word_search) < len(word_user):
-//             return {"code" : -1, "comment" : "Votre mot est trop grand"}
-//         else:
-//             return {"code" : 0, "comment" : "Votre mot est de bonne taille"}
-
-// function check(self, word_user):
-//         if type(word_user) is str:
-//             length = self.world_length(word_user)
-//             hint = self.insinuate(word_user, length["code"])
-//             self.think_stupidly(word_user, hint, length["comment"])
-//         else:
-//             print("Not string")
-  
-// function think_stupidly(self, word_user, hint, comment):
-//         self.memory.append({"word" : word_user, "hint" : hint, "comment" : comment})
